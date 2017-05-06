47b91b90e48aa52409520358509c573965275898
Marc Maguire <maguire.marc@gmail.com>
2017-05-06 12:18:31 -0400

    initial commit

---
+++ Snakes & Ladders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Snakes & Ladders
+//
+//  Created by Marc Maguire on 2017-05-06.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
